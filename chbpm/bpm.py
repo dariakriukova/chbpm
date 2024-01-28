9e039735a609e021c4a77ae0d4d5ad8b1f87855c
Daria Kriukova <daria.kriukova@elfys.fi>
2024-01-28 20:25:31 +0200

    - librosa is used to load audio
    - error message about depricated function is ignored
    
    - all audio formats within library supported
    
    - launch.json for debugging added

--- chbpm/bpm.py
+++ chbpm/bpm.py
@@ -5,47 +5,58 @@ import shutil
 import subprocess
 import traceback
 import numpy as np
+import warnings
+warnings.filterwarnings('ignore', category=FutureWarning, module='librosa.core.audio')
+warnings.filterwarnings('ignore')
+
 
 def analyze_bpm(file_path):
-    with audioread.audio_open(file_path) as f:
-        audio_data = []
-        for buffer in f:
-            audio_data.append(np.frombuffer(buffer, dtype=np.int16))
-
-        y = np.concatenate(audio_data)
-        if f.channels > 1:
-            y = y.reshape(-1, f.channels)
-            y = np.mean(y, axis=1)
-
-    y = y.astype(np.float32) / np.iinfo(np.int16).max
-
-    tempo, _ = librosa.beat.beat_track(y=y, sr=f.samplerate)
-    return tempo
+    try:
+        y, sr = librosa.load(file_path, sr=None, mono=True)
+        tempo, _ = librosa.beat.beat_track(y=y, sr=sr)
+        return tempo
+    except audioread.NoBackendError:
+        return None
 
 
 def adjust_tempo_ffmpeg(input_file, output_file, original_bpm, target_bpm):
     tempo_factor = target_bpm / original_bpm
-    command = ['ffmpeg', '-i', input_file, '-filter:a', f'atempo={tempo_factor}', output_file]
+    command = [
+    'ffmpeg', '-i', input_file, 
+    '-filter:a', f'atempo={tempo_factor}', 
+    '-c:a', 'libfdk_aac', '-vbr', '4', 
+    '-c:v', 'copy', '-map', '0', 
+    output_file
+    ]
+    print(' '.join(command))
     subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 def process_audio_files(source_folder, output_folder):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
-
     for root, dirs, files in os.walk(source_folder):
         for file_name in files:
-            if file_name.startswith('.') or not file_name.lower().endswith(('.mp3', '.wav', '.m4a')):
-                continue
-
             file_path = os.path.join(root, file_name)
 
             try:
+                print(f"Analyzing '{file_name}'", end=' ')
                 bpm = analyze_bpm(file_path)
-                print(f"Analyzing '{file_name}' - BPM: {bpm}")
-
+                if bpm is None:
+                    print()
+                    continue
+                print(f" - BPM: {bpm}")
+                
                 if 80 <= bpm < 100 or 147 <= bpm <= 210:
                     target_bpm = 90 if bpm < 100 else 180
-                    output_path = os.path.join(output_folder, file_name)
+                    
+                    base_name, _ = os.path.splitext(file_name)
+                    relative_path = os.path.relpath(root, source_folder)
+                    new_dir = os.path.join(output_folder, relative_path)
+                    if not os.path.exists(new_dir):
+                        os.makedirs(new_dir)
+
+                    output_path = os.path.join(new_dir, base_name + '.m4a')
+                    
                     print(f"Adjusting '{file_name}' from BPM: {bpm} to {target_bpm}")
                     adjust_tempo_ffmpeg(file_path, output_path, bpm, target_bpm)
 
