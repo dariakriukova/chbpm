ee21bba34d90af51d2de8a6511867734f677aba7
Daria Kriukova <daria.kriukova@elfys.fi>
2024-01-30 10:04:21 +0200

    - Unused imports removed. - Formatting added.

--- chbpm/__main__.py
+++ chbpm/__main__.py
@@ -1,23 +1,42 @@
-import sys
 import chbpm.bpm as bpm
 import argparse
 
 
 def main():
     parser = argparse.ArgumentParser(description="Process audio files to adjust BPM.")
-    
-    parser.add_argument('input_path', help="Path to the input music folder")
-    parser.add_argument('-o', '--output_path', default='adjusted', help="Path to the output folder")
-    parser.add_argument('-t', '--target_bpm', type=int, default=180, help="Optional target BPM for tempo adjustment")
-    parser.add_argument('-r', '--range_percentage', type=float, default=15, help="Percentage for BPM range adjustment (default: 15)")
-    parser.add_argument('--ffmpeg_options', help="Additional FFmpeg command line options")
+
+    parser.add_argument("input_path", help="Path to the input music folder")
+    parser.add_argument(
+        "-o", "--output_path", default="adjusted", help="Path to the output folder"
+    )
+    parser.add_argument(
+        "-t",
+        "--target_bpm",
+        type=int,
+        default=180,
+        help="Optional target BPM for tempo adjustment",
+    )
+    parser.add_argument(
+        "-r",
+        "--range_percentage",
+        type=float,
+        default=15,
+        help="Percentage for BPM range adjustment (default: 15)",
+    )
+    parser.add_argument(
+        "--ffmpeg_options", help="Additional FFmpeg command line options"
+    )
 
     args = parser.parse_args()
-    
-    
-    
-    
-    bpm.main(args.input_path, args.output_path, args.target_bpm, args.range_percentage, args.ffmpeg_options)
+
+    bpm.main(
+        args.input_path,
+        args.output_path,
+        args.target_bpm,
+        args.range_percentage,
+        args.ffmpeg_options,
+    )
+
 
 if __name__ == "__main__":
     main()
