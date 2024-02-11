eb2814d8cda15bc4a224f80470394684d43a7eb4
Daria Kriukova <daria.kriukova@elfys.fi>
2024-02-11 13:41:05 +0200

    Unit test to check the bpm of metronome's beat.

---
+++ tests/test_analyze_bpm.py
@@ -0,0 +1,21 @@
+import pytest
+from chbpm.bpm import analyze_bpm
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+test_cases = [
+    ('tests/data/60.m4a', 60),
+    ('tests/data/70.m4a', 70),
+    ('tests/data/90.m4a', 90),
+    ('tests/data/100.m4a', 100),
+    ('tests/data/120.m4a', 120),
+    ('tests/data/180.m4a', 180),
+]
+
+@pytest.mark.parametrize("file_path, expected_bpm", test_cases)
+def test_analyze_bpm_known_file(file_path, expected_bpm):
+    tolerance = 5
+    bpm = analyze_bpm(file_path, expected_bpm)
+    assert expected_bpm - tolerance <= bpm <= expected_bpm + tolerance, f"Expected BPM to be within {expected_bpm - tolerance} to {expected_bpm + tolerance}, got {bpm}"
+    logging.info(f"Detected BPM: {bpm}")
